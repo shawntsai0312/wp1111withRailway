ec10e88b2c47c74194bd50aa289fdf60a0aecbf4
shawntsai0312 <shawntsai0312@gmail.com>
2022-12-24 14:25:30 +0800

    1426

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_ROOT = process.env.NODE_ENV === "production" ? "/api" : "http://localhost:4000/";
+const API_ROOT = process.env.NODE_ENV === "production" ? "" : "http://localhost:4000/";
 
 const instance = axios.create({
   baseURL: API_ROOT
