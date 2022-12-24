24026fa711899b3faaec76829b990d315dc27db9
shawntsai0312 <shawntsai0312@gmail.com>
2022-12-24 14:12:15 +0800

    1412test

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,15 +2,27 @@ import express from 'express';
 import cors from 'cors';
 import db from './db';
 import routes from './routes';
+import path from 'path';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 db.connect();
 
-app.use(cors());
+if (process.env.NODE_ENV === "development") {
+    app.use(cors());
+}
+
 app.use(express.json());
 app.use('/', routes);
+
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, "../frontend", "build")))
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"))
+    })
+}
 
 app.listen(port, () =>
     console.log(`Example app listening on port ${port}!`),
